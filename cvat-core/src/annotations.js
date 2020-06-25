4a3b09994e21d3b2e206171ec4cff7376730be58
Boris Sekachev <40690378+bsekachev@users.noreply.github.com>
2020-06-25 17:47:28 +0300

    [CVAT-UI] Data annotation team requests and some fixes (#1753)

--- cvat-core/src/annotations.js
+++ cvat-core/src/annotations.js
@@ -77,6 +77,15 @@
         }
     }
 
+    async function closeSession(session) {
+        const sessionType = session instanceof Task ? 'task' : 'job';
+        const cache = getCache(sessionType);
+
+        if (cache.has(session)) {
+            cache.delete(session);
+        }
+    }
+
     async function getAnnotations(session, frame, allTracks, filters) {
         const sessionType = session instanceof Task ? 'task' : 'job';
         const cache = getCache(sessionType);
@@ -382,5 +391,6 @@
         redoActions,
         clearActions,
         getActions,
+        closeSession,
     };
 })();
